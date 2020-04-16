0acf55839882916dd6f8b7748ac2a269b60dabce
Fabrizio Rinaldi <linuz90@gmail.com>
2020-04-16 10:10:19 +0200

    Cards rendering improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -148,141 +148,136 @@ const PageContent = () => {
 
               const key = `${tag}${author.name}${index}${show}`;
 
+              if (!show) return null;
+
               return (
-                <Fragment key={key}>
-                  <AnimateSharedLayout show={show} id={key}>
-                    <Card inline width style={{ marginBottom: "20px" }}>
-                      <HStack
-                        align="right"
-                        vAlign="center"
-                        style={{ position: "absolute", top: "9px", right: "9px" }}
-                        gap="4px"
+                <AnimatedDiv key={key} id={key}>
+                  <Card inline width style={{ marginBottom: "20px" }}>
+                    <HStack
+                      align="right"
+                      vAlign="center"
+                      style={{ position: "absolute", top: "9px", right: "9px" }}
+                      gap="4px"
+                    >
+                      {source_url && (
+                        <a href={source_url} target="_blank" rel="noopener noreferrer" style={{ borderBottom: "none" }}>
+                          <Icon name="link" color={config.colors.c4} size="16px" />
+                        </a>
+                      )}
+                      <Tag
+                        tag={tag}
+                        onClick={() => {
+                          handleSelectTag(tag === currentTag ? "all" : tag);
+                        }}
+                      />
+                    </HStack>
+                    {author && (
+                      <a href={url} target="_blank" rel="noopener noreferrer" style={{ borderBottom: "none" }}>
+                        <HStack gap="4px" noWrap>
+                          <img
+                            src={`https://unavatar.now.sh/${author.avatar}`}
+                            alt={author.name}
+                            css={css`
+                              width: 46px;
+                              height: 46px;
+                              border-radius: 50%;
+                            `}
+                          />
+                          <VStack gap={0}>
+                            <H4>{author.name}</H4>
+                            <P2 noWrap>{author.bio}</P2>
+                          </VStack>
+                        </HStack>
+                      </a>
+                    )}
+                    {body && (
+                      <P1 style={{ marginTop: "1em", whiteSpace: "pre-line" }}>
+                        <Linkify>{body}</Linkify>
+                      </P1>
+                    )}
+                    {preview_image && (
+                      <img
+                        style={{
+                          marginTop: "1em",
+                          maxWidth: "100%",
+                          borderRadius: "6px",
+                          border: `1px solid ${config.colors.uiBorderColor}`,
+                          maxHeight: "240px",
+                          objectFit: "cover",
+                          objectPosition: "top"
+                        }}
+                        src={preview_image}
+                        alt="Preview"
+                      />
+                    )}
+                    {offer && (
+                      <a
+                        href={offer.url}
+                        style={{ marginTop: "1em", borderBottom: "none" }}
+                        target="_blank"
+                        rel="noopener noreferrer"
                       >
-                        {source_url && (
-                          <a
-                            href={source_url}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            style={{ borderBottom: "none" }}
-                          >
-                            <Icon name="link" color={config.colors.c4} size="16px" />
-                          </a>
-                        )}
-                        <Tag
-                          tag={tag}
-                          onClick={() => {
-                            handleSelectTag(tag === currentTag ? "all" : tag);
-                          }}
-                        />
-                      </HStack>
-                      {author && (
-                        <a href={url} target="_blank" rel="noopener noreferrer" style={{ borderBottom: "none" }}>
-                          <HStack gap="4px" noWrap>
-                            <img
-                              src={`https://unavatar.now.sh/${author.avatar}`}
-                              alt={author.name}
-                              css={css`
-                                width: 46px;
-                                height: 46px;
-                                border-radius: 50%;
-                              `}
-                            />
+                        <SubtleCard color="hsl(12, 100%, 62%)">
+                          <HStack gap="0.5em">
+                            <Icon size="2em" name="couponBold" color="hsl(12, 100%, 62%)" />
                             <VStack gap={0}>
-                              <H4>{author.name}</H4>
-                              <P2 noWrap>{author.bio}</P2>
+                              <H4 color="hsl(12, 30%, 20%)">{offer.title}</H4>
+                              <P2 color="hsl(12, 100%, 62%)">{offer.subtitle}</P2>
                             </VStack>
                           </HStack>
-                        </a>
-                      )}
-                      {body && (
-                        <P1 style={{ marginTop: "1em", whiteSpace: "pre-line" }}>
-                          <Linkify>{body}</Linkify>
-                        </P1>
-                      )}
-                      {preview_image && (
-                        <img
-                          style={{
-                            marginTop: "1em",
-                            maxWidth: "100%",
-                            borderRadius: "6px",
-                            border: `1px solid ${config.colors.uiBorderColor}`,
-                            maxHeight: "240px",
-                            objectFit: "cover",
-                            objectPosition: "top"
-                          }}
-                          src={preview_image}
-                          alt="Preview"
-                        />
-                      )}
-                      {offer && (
-                        <a
-                          href={offer.url}
-                          style={{ marginTop: "1em", borderBottom: "none" }}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <SubtleCard color="hsl(12, 100%, 62%)">
-                            <HStack gap="0.5em">
-                              <Icon size="2em" name="couponBold" color="hsl(12, 100%, 62%)" />
-                              <VStack gap={0}>
-                                <H4 color="hsl(12, 30%, 20%)">{offer.title}</H4>
-                                <P2 color="hsl(12, 100%, 62%)">{offer.subtitle}</P2>
-                              </VStack>
-                            </HStack>
-                          </SubtleCard>
-                        </a>
-                      )}
-                      {url && (
-                        <StyledA
-                          style={{ marginTop: "1em" }}
-                          type="primary"
-                          href={url}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          {label || "Get it"}
-                        </StyledA>
-                      )}
-                      {chrome_extension && (
-                        <a
-                          href={chrome_extension.url}
-                          style={{ marginTop: "1em", borderBottom: "none" }}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <SubtleCard color="rgb(80,80,80)">
-                            <Grid columns="3em 1fr auto" gap="0.5em" vAlign="center">
-                              <img src={chrome_extension.icon} style={{ width: "3em" }} alt={chrome_extension.title} />
-                              <VStack gap={0}>
-                                <H4 color="hsl(12, 30%, 20%)" noWrap>
-                                  {chrome_extension.title}
-                                </H4>
-                                <P2 noWrap>Add to Google Chrome</P2>
-                              </VStack>
-                              <span
-                                css={css`
-                                  display: inline-block;
-                                  background: #4285f4;
-                                  padding: 1px 12px;
-                                  border-radius: 5px;
-                                  p {
-                                    font-size: 16px;
-                                    font-weight: 500;
-                                  }
-                                  @media (max-width: 640px) {
-                                    display: none;
-                                  }
-                                `}
-                              >
-                                <P2 color="white">INSTALL</P2>
-                              </span>
-                            </Grid>
-                          </SubtleCard>
-                        </a>
-                      )}
-                    </Card>
-                  </AnimateSharedLayout>
-                </Fragment>
+                        </SubtleCard>
+                      </a>
+                    )}
+                    {url && (
+                      <StyledA
+                        style={{ marginTop: "1em" }}
+                        type="primary"
+                        href={url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {label || "Get it"}
+                      </StyledA>
+                    )}
+                    {chrome_extension && (
+                      <a
+                        href={chrome_extension.url}
+                        style={{ marginTop: "1em", borderBottom: "none" }}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <SubtleCard color="rgb(80,80,80)">
+                          <Grid columns="3em 1fr auto" gap="0.5em" vAlign="center">
+                            <img src={chrome_extension.icon} style={{ width: "3em" }} alt={chrome_extension.title} />
+                            <VStack gap={0}>
+                              <H4 color="hsl(12, 30%, 20%)" noWrap>
+                                {chrome_extension.title}
+                              </H4>
+                              <P2 noWrap>Add to Google Chrome</P2>
+                            </VStack>
+                            <span
+                              css={css`
+                                display: inline-block;
+                                background: #4285f4;
+                                padding: 1px 12px;
+                                border-radius: 5px;
+                                p {
+                                  font-size: 16px;
+                                  font-weight: 500;
+                                }
+                                @media (max-width: 640px) {
+                                  display: none;
+                                }
+                              `}
+                            >
+                              <P2 color="white">INSTALL</P2>
+                            </span>
+                          </Grid>
+                        </SubtleCard>
+                      </a>
+                    )}
+                  </Card>
+                </AnimatedDiv>
               );
             })}
           </Columns>
@@ -342,17 +337,16 @@ const Tag = ({ tag, onClick }) => {
   );
 };
 
-export const AnimateSharedLayout = ({ show, id, children }) =>
-  show && (
-    <motion.div
-      layoutId={id}
-      initial={{ opacity: 0, transform: "scale(0.8)" }}
-      animate={{ opacity: 1, transform: "scale(1)" }}
-      exit={{ opacity: 0, transform: "scale(0.8)" }}
-    >
-      {children}
-    </motion.div>
-  );
+export const AnimatedDiv = ({ show, id, children }) => (
+  <motion.div
+    layoutId={id}
+    initial={{ opacity: 0, transform: "scale(0.8)" }}
+    animate={{ opacity: 1, transform: "scale(1)" }}
+    exit={{ opacity: 0, transform: "scale(0.8)" }}
+  >
+    {children}
+  </motion.div>
+);
 
 const Logo = () => (
   <svg width="96" height="96" viewBox="0 0 96 96" fill="none" xmlns="http://www.w3.org/2000/svg">
