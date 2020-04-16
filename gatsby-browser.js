5f6c253ad28b49a22afb85b71939508cfd62869a
Francesco Di Lorenzo <fradilorenzo92@gmail.com>
2020-04-16 12:40:27 +0200

    Don't scroll on navigation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+exports.shouldUpdateScroll = ({ routerProps: { location }, getSavedScrollPosition }) => {
+  // don't scroll when navigating
+  return false;
+};
